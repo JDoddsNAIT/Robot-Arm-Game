b733e7264c22a0db738858b4f79a9977cf66622b
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2026-01-15 11:28:05 -0700

    bruh

--- Assets/Features/Runtime/LogicGates/GateOutput.cs
+++
@@ -1,9 +0,0 @@
-namespace Features.LogicGates
-{
-	public class GateOutput : GateConnector
-	{
-		[SerializeField] private float _value;
-
-		public override float Value { get => _value; set => _value = value; }
-	}
-}
--- Assets/Features/Runtime/UI/BuildArea.cs
+++ Assets/Features/Runtime/UI/BuildArea.cs
@@ -1,11 +1,15 @@
+using Features.Persistence;
 using UnityEngine;
 
 namespace Features.UI
 {
 	public class BuildArea : MonoBehaviour
 	{
+		[SerializeField] private SerializableGuid _levelId = Guid.NewGuid();
 		[SerializeField] private Transform _spawnArea;
 
+		public SerializableGuid Id => _levelId;
+		
 		public void OnDropLogicGate(GameObject gameObject)
 		{
 			if (gameObject.TryGetComponent(out ToolboxLogicGate component))
